30963fa30c9760f8e951f2b2093e2bf8bb229f23
ErauQsPhan <mrsplol102@gmail.com>
2024-02-01 22:26:59 +0700

    react with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,13 @@
-// import { useState } from 'react'
-import "./App.css";
-import { updateTodo } from "./utils/utilitys";
-// import { ranker } from "./utils/generics";
-// import { devices, getDevicesKeys } from "./utils/generic-keyof";
-// const reviews: {
-//   name: string;
-//   stars: number;
-//   premiumUser: boolean;
-//   date: string;
-// }[] = [
-//   {
-//     name: "ErauQs",
-//     stars: 4,
-//     premiumUser: true,
-//     date: "27/04/2023",
-//   },
-//   {
-//     name: "Gintama",
-//     stars: 10,
-//     premiumUser: false,
-//     date: "22/04/2023",
-//   },
-//   {
-//     name: "Phuong Binh",
-//     stars: 40,
-//     premiumUser: true,
-//     date: "09/04/2023",
-//   },
-// ];
+const Heading = ({ title }: { title: string }) => {
+  return <h2 className="font-bold text-2xl mb-5">{title}</h2>;
+};
 
-// const travelItem: {
-//   image: string;
-//   name: string;
-//   totalReviews: number;
-//   rating: number;
-//   location: string;
-//   price: number;
-//   date: string;
-//   depature: string;
-//   feature: {
-//     wifi: boolean;
-//     parking: boolean;
-//     offer: boolean;
-//   };
-// }[] = [
-//   {
-//     image: "https://wallpapercave.com/wp/wp4111669.jpg",
-//     name: "Hạ Long Bay",
-//     totalReviews: 2450,
-//     rating: 4.6,
-//     location: "Hải Phòng",
-//     price: 400000,
-//     date: "27/1/2024",
-//     depature: "Sài gòn",
-//     feature: {
-//       wifi: true,
-//       parking: true,
-//       offer: true,
-//     },
-//   },
-// ];
-// console.log(travelItem);
-
-// type Age = 18 | 25 | 30;
-
-console.log(
-  updateTodo({ title: "Learn Javascript", description: "6 month" }, {})
-);
-function App() {
-  // const [count, setCount] = useState(0)
-  // const [valueStr, setValueStr] = simpleUseState("ErauQs");
-  // console.log(valueStr);
-  // setValueStr("binh");
-  // console.log(valueStr);
-
-  // function displayReviewInfo(
-  //   name: string,
-  //   totalReview: number,
-  //   premium?: boolean
-  // ) {
-  //   return (
-  //     <div className="review-info">
-  //       <Card description="" title=""></Card>
-  //       Review total <strong>{totalReview}</strong> | Last review by{" "}
-  //       <strong>{name}</strong>
-  //       {premium ? "*" : ""}
-  //     </div>
-  //   );
-  // }
-
-  // const users: {
-  //   fisrtname: string;
-  //   lastName: string;
-  //   age: Age;
-  //   isTeacher: boolean;
-  //   hobbies: (string | number)[];
-  //   contact: [number, string];
-  //   permission: Permission;
-  // } = {
-  //   fisrtname: "Phan",
-  //   lastName: "Binh",
-  //   age: 25,
-  //   isTeacher: true,
-  //   hobbies: ["playgame", "music", 50],
-  //   contact: [12124, "binh@gmail.com"],
-  //   permission: Permission.ADMIN,
-  // };
-  // console.log(users);
-
-  return <></>;
-}
+const App = () => {
+  return (
+    <div className="p-5">
+      <Heading title="Todo App"></Heading>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/utils/search-param-decoding.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const query = "/home?name=erauqs&age=26";
+
+// const objQueryResult : ABCType = {
+//     name: "erauqs",
+//     age: 26
+// }
+
+// Khó quá để từ từ
