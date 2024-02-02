e492f4798e2a618c8019463b78d86a59410a527c
ErauQsPhan <mrsplol102@gmail.com>
2024-02-02 12:16:03 +0700

    todo App with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,86 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useReducer, useRef } from "react";
+
 const Heading = ({ title }: { title: string }) => {
-  return <h2 className="font-bold text-2xl mb-5">{title}</h2>;
+  return <h2 className="mb-5 text-2xl font-bold">{title}</h2>;
 };
 
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
+
+interface Todo {
+  id: number;
+  text: string;
+}
+const todoReducer = (state: Todo[], action: ActionType) => {
+  switch (action.type) {
+    case "ADD":
+      return [
+        ...state,
+        {
+          id: state.length,
+          text: action.text,
+        },
+      ];
+    case "REMOVE":
+      return state.filter((todo: Todo) => todo.id !== action.id);
+    default:
+      throw new Error("");
+  }
+};
+
+const initialValue: Todo[] = [];
+
 const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialValue);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onRemoveTodo = (todoId: number) => {
+    dispatch({
+      type: "REMOVE",
+      id: todoId,
+    });
+  };
+  const onAddTodo = () => {
+    if (inputRef.current) {
+      dispatch({
+        type: "ADD",
+        text: inputRef.current.value,
+      });
+    }
+  };
   return (
     <div className="p-5">
       <Heading title="Todo App"></Heading>
+      <div className="max-w-sm">
+        <div className="flex flex-col mb-5 gap-y-2">
+          {todos.map((todo) => (
+            <div className="flex items-center gap-x-3" key={todo.id}>
+              <span>{todo.text}</span>
+              <button
+                onClick={() => onRemoveTodo(todo.id)}
+                className="p-2 text-sm text-white bg-red-500 rounded-lg "
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center gap-x-5">
+          <input
+            type="text"
+            className="p-4 border rounded outline-none border-slate-200"
+            ref={inputRef}
+          />
+          <button
+            onClick={onAddTodo}
+            className="p-4 text-center text-white bg-blue-500 rounded-lg"
+          >
+            Add todo
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
